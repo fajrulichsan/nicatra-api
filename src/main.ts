c538e47b851284e573abbd0b35fc8f9bc85e8da6
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2025-05-17 11:23:31 +0700

    add prefix /api

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3100);
 }
 bootstrap();
