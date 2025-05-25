c294b5d18bc5d0ddb3816ab25789ae31b97d8d82
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2025-05-25 23:22:37 +0700

    fix station

--- src/station/station.service.ts
+++ src/station/station.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Station } from './entities/station.entity';  
 import { InjectRepository } from '@nestjs/typeorm';
@@ -16,16 +16,13 @@ export class StationService {
   async getAllActiveStations(): Promise<Station[]> {
     try {
       this.logger.log('Fetching all active stations where statusData is true');
-      const result =  await this.stationRepo.find({
-        where: { statusData: true },
-        order: { name: 'ASC' },
-      });
+      const result =  await this.stationRepo.find();
 
       console.log('Active stations fetched successfully:', result);
       return result;
     } catch (error) {
       this.logger.error('Failed to fetch active stations', error.stack);
-      throw new InternalServerErrorException('Failed to fetch active stations');
+      throw new HttpException('Failed to fetch active stations', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
